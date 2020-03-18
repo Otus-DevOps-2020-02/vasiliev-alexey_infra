d7d0601114abacc38e567d13e1852e1ba0c4b842
vasiliev-alexey <alex_v81@MAIL.RU>
2020-03-18 23:35:20 +0300

    fix space and dirs

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
 sudo apt update
 sudo apt install -y ruby-full ruby-bundler build-essential
-
-
-ruby -v
-#ruby 2.3.1p112 (2016-04-26) [x86_64-linux-gnu]
-bundler -v
-#Bundler version 1.11.2
