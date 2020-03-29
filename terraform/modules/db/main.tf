18ce8157b18da077000615f384ef8516d550f634
vasiliev-alexey <alex_v81@MAIL.RU>
2020-03-29 17:50:47 +0300

    доступ к БД по внутрнненнему  IP

--- terraform/modules/db/main.tf
+++ terraform/modules/db/main.tf
@@ -45,5 +45,5 @@ resource "google_compute_firewall" "firewall_mongo" {
     ports = ["27017"]
   }
   target_tags = ["reddit-db"]
-   source_ranges = ["0.0.0.0/0"]
+   source_tags = ["reddit-app"]
 }
--- terraform/prod/outputs.tf
+++ terraform/prod/outputs.tf
@@ -4,3 +4,8 @@ output "app_external_ip" {
 output "db_external_ip" {
    value = "${module.db.db_external_ip}"
 }
+output "db_internal_ip" {
+   value = "${module.db.db_internal_ip}"
+}
+
+
