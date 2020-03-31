d78262a687d66eff9625192350384d3d175f0880
vasiliev-alexey <alex_v81@MAIL.RU>
2020-03-31 14:02:33 +0300

    fix from linter

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,10 +1,10 @@
-provider "google" {
-  version = "~> 2.15"
-  project = var.project
-  region  = var.region
-  zone    = var.zone
-
-}
+#provider "google" {
+#  version = "~> 2.15"
+#  project = var.project
+#  region  = var.region
+#  zone    = var.zone
+#
+#}
 
 module "storage-bucket" {
   source  = "SweetOps/storage-bucket/google"
