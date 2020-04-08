9936ec6cfb7daf8dff50022576123daefb7cc1b2
vasiliev-alexey <alex_v81@MAIL.RU>
2020-04-08 16:24:15 +0300

    fix  HW*

---
+++ ansible/dynamic_inv.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters"
+fi
+
+if [[ $1 == '--host' ]]
+then
+    echo "[]"
+elif    [[ $1  == "--list" ]]
+then
+    cat inventory.json
+else
+
+    echo  $1 " wrong usage posible --host --list"
+fi
